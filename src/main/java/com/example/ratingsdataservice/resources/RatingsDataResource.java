a264ef3a170460c34635ca4da5ad44cdc5e74183
Thomas Lee <tjl352@gmail.com>
2019-09-28 21:35:19 -0400

    create UserRating

--- src/main/java/com/example/ratingsdataservice/resources/RatingsDataResource.java
+++ src/main/java/com/example/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,9 +1,13 @@
 package com.example.ratingsdataservice.resources;
 
 import com.example.ratingsdataservice.models.Rating;
+import com.example.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +16,14 @@ public class RatingsDataResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("123", 4),
+                new Rating("456", 3)
+        );
+        return new UserRating(ratings);
     }
 }
